c5adc58308dc2fb6b8e7cfbce8f35e39530656a3
DrorGaon <drordtuii@gmail.com>
2024-10-29 22:03:22 +0200

    Add logger service

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { bugService } from './services/bug-service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
@@ -8,12 +9,20 @@ app.get('/', (req, res) => res.send('Hello there'))
 app.get('/api/bug', (req, res) => {
     bugService.query()
         .then((bugs) => res.send(bugs))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(500).send('Problem getting bugs')
+        })
 })
 
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
     bugService.get(bugId)
         .then((bug) => res.send(bug))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(500).send('Problem getting bug')
+        })
 })
 
-app.listen(3030, () => console.log('Server listening on port http://127.0.0.1:3030/'))
+app.listen(3030, () => loggerService.info('Server listening on port http://127.0.0.1:3030/'))
