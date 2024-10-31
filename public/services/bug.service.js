a927503f0980d18b124bf3dc34a4feae5cad2e64
DrorGaon <drordtuii@gmail.com>
2024-10-31 19:44:42 +0200

    Add service error handling

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -8,24 +8,31 @@ export const bugService = {
 }
 
 function query() {
-    return axios.get(BASE_URL)
+    return axios
+        .get(BASE_URL)
         .then(res => res.data)
+        .catch(err => {throw err.response.data})
 }
 function getById(bugId) {
-    return axios.get(BASE_URL + bugId)
+    return axios
+        .get(BASE_URL + bugId)
         .then(res => res.data)
         .catch(err => {throw err.response.data})
 }
 
 function remove(bugId) {
-    return axios.get(BASE_URL + bugId + '/remove')
-        .then(res => res.data) 
+    return axios
+        .get(BASE_URL + bugId + '/remove')
+        .then(res => res.data)
+        .catch(err => {throw err.response.data})
 }
 
 function save(bug) {
     const url = BASE_URL + 'save'
     let queryParams = `?title=${bug.title}&description=${bug.description}&severity=${bug.severity}`
     if (bug._id) queryParams += `&_id=${bug._id}`
-    return axios.get(url + queryParams)
+    return axios
+        .get(url + queryParams)
         .then(res => res.data) 
+        .catch(err => {throw err.response.data})
 }
