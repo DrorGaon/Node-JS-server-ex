ac0715a9b9b00d2e17c2020f249db4909a865417
DrorGaon <drordtuii@gmail.com>
2024-10-31 15:52:00 +0200

    Add bug descriptions

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -13,7 +13,6 @@ export const bugService = {
     remove,
 }
 
-
 function query() {
     return storageService.query(STORAGE_KEY)
 }
@@ -33,37 +32,35 @@ function save(bug) {
     }
 }
 
-
-
-
 function _createBugs() {
     let bugs = utilService.loadFromStorage(STORAGE_KEY)
     if (!bugs || !bugs.length) {
         bugs = [
             {
-                title: "Infinite Loop Detected",
+                title: 'Infinite Loop Detected',
                 severity: 4,
-                _id: "1NF1N1T3"
+                _id: '1NF1N1T3',
+                description: 'Proccess went into infinite loop and was killed.',
             },
             {
-                title: "Keyboard Not Found",
+                title: 'Keyboard Not Found',
                 severity: 3,
-                _id: "K3YB0RD"
+                _id: 'K3YB0RD',
+                description: 'User has no keyboard.',
             },
             {
-                title: "404 Coffee Not Found",
+                title: '404 Coffee Not Found',
                 severity: 2,
-                _id: "C0FF33"
+                _id: 'C0FF33',
+                description: 'Developer doesn\'t have coffee, major delays expected.',
             },
             {
-                title: "Unexpected Response",
+                title: 'Unexpected Response',
                 severity: 1,
-                _id: "G0053"
+                _id: 'G0053',
+                description: 'What do you mean? omg this is so unexpected.',
             }
         ]
         utilService.saveToStorage(STORAGE_KEY, bugs)
     }
-
-
-
 }
